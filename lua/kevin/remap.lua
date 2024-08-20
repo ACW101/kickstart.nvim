674bf307149b877844ef96d7ff900538086cf3de
Kevin Wang <kevinwang101@gmail.com>
2024-08-20 08:56:31 -0700

    first commit

---
+++ lua/kevin/remap.lua
@@ -0,0 +1,22 @@
+-- move highlighted line up and down
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
+-- keep join two line curser in the same place
+vim.keymap.set('n', 'J', 'mzJ`z')
+
+-- jump up and down half page and keep view in the middle
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+
+-- when search keep view in the middle
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
+vim.keymap.set('n', '<leader>zig', '<cmd>LspRestart<cr>')
+
+-- greatest remap ever
+vim.keymap.set('x', '<leader>p', [["_dP]])
+
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]])
+vim.keymap.set('n', '<leader>Y', [["+Y]])
